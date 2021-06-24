8c404edf8b8efbb3ede258faf2ec7dd6b9083c72
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-24 15:56:45 +0200

    Stampati album tramite vueJS

--- Milestone-2/ASSETS/JS/app.js
+++ Milestone-2/ASSETS/JS/app.js
@@ -2,23 +2,18 @@ const app = new Vue({
     /* Options object */
     el: '#app',
     data: {
-      products: null
+      musicData: []
     },
     methods: {
   
     },
     mounted() {
-
-       console.log('prova');
-       axios.get('/api/dischi.php')
+       axios.get('/php/Esercizi_PHP/Esercizio_47/php-ajax-dischi/Milestone-2/api/dischi.php')
         .then(resp => {
           console.log(resp);
-          this.products = resp.data
+          this.musicData = resp.data
         }).catch(e => {
           console.log(e);
         })
     }
   })
-
-
-  console.log('prova');
--- Milestone-2/index.php
+++ Milestone-2/index.php
@@ -4,34 +4,36 @@
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css "integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w== "crossorigin="anonymous" />
+    <!-- Link CSS -->
     <link rel="stylesheet" href="./ASSETS/CSS/style.css">
-    <title>Document</title>
+    <!-- Title -->
+    <title>Dischi PHP</title>
 </head>
 <body>
 
     <div id="app">
 
+        <header>
+            <i class="fab fa-spotify"></i>
+        </header>
+
         <main>
             <div class="container">
 
-                <?php
-                foreach ($dischi as $elements) { ?>
-                    <div class="album">
-
-                        <img src="<?php echo($elements['poster']); ?>" alt="copertine album">
-                        <h2><?php echo($elements['title']); ?></h2>
-                        <h5><?php echo($elements['author']); ?></h5>
-                        <h6><?php echo($elements['year']); ?></h6>
-                            
-                    </div>
-                <?php } ?>
+                <div class="album "v-for="album in musicData">
+                    <img :src="album.poster" alt="copertine album">
+                    <h2>{{album.title}}</h2>
+                    <h5>{{album.author}}</h5>
+                    <h6>{{album.year}}</h6>
+                </div>
 
             </div>
         </main>
